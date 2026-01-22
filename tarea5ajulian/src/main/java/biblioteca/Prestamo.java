c3d258b6b0107413b9629bb5708b4cc107a42775
julianm30 <jmoesp768@g.educaand.es>
2026-01-22 10:32:46 +0100

    se añaden getters y setters

--- tarea5ajulian/src/main/java/biblioteca/Prestamo.java
+++ tarea5ajulian/src/main/java/biblioteca/Prestamo.java
@@ -1,23 +1,72 @@
 package biblioteca;
 
+import java.time.LocalDate;
+
 public class Prestamo {
+
+    private final int id;
+
+    private Estudiante estudiante;
+    private Libro libro;
+    private LocalDate fechaInicial;
+    private LocalDate fechaDev;
+
+
+    public Prestamo(int id, Estudiante estudiante, Libro libro, LocalDate fechaInicial) {
+        this.id = id;
+        this.estudiante = estudiante;
+        this.libro = libro;
+        this.fechaInicial = fechaInicial;
+        
+        //fechaDev 15 dias desp de la inicial   
+        this.fechaDev = fechaInicial.plusDays(15);
+    }
+    //Sobrecarga de constructores
+public Prestamo(int id, Estudiante estudiante, Libro libro ) {
+        this.id = id;
+        this.estudiante = estudiante;
+        this.libro = libro;
+        this.fechaInicial = LocalDate.now();
+        
+        //fechaDev 15 dias desp de la inicial
+        this.fechaDev = fechaInicial.plusDays(15);
+    }
+public int getId() {
+    return id;
+}
+public Estudiante getEstudiante() {
+    return estudiante;
+}
+public Libro getLibro() {
+    return libro;
+}
+public LocalDate getFechaInicial() {
+    return fechaInicial;
+}
+public LocalDate getFechaDev() {
+    return fechaDev;
+}
+public void setEstudiante(Estudiante estudiante) {
+    this.estudiante = estudiante;
+}
+public void setLibro(Libro libro) {
+    this.libro = libro;
+}
+public void setFechaInicial(LocalDate fechaInicial) {
+    this.fechaInicial = fechaInicial;
+    this.fechaDev = fechaInicial.plusDays(15);
+    //para que se cumpla los 15 dias
+}
+
 
     
 
 
-    /*
-     * tiene como atributos privados
-     * id(numerico)
-     * un estudiante
-     * un libro
-     * localdate -> fecha ini / fecha devolucion=esto es 15 dias despues
-     * se le ppone getter y setters
-     */
+
+  
 
 
-    //mirar  clase collections.sort
-            // colletions.binarysearch
-
+   
 
 
 
